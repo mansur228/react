27a06633920f51740d0aac11899c30a1bb72b08a
mansur228 <60790177+mansur228@users.noreply.github.com>
2021-04-29 00:15:32 +0600

    header remake

--- src/App/Header/Header.js
+++ src/App/Header/Header.js
@@ -7,7 +7,21 @@ export default class Header extends Component {
         return (
             <header id='Header'>
                 <div className='container'>
-
+                    <h1 className={s.title}>
+                        <span>Не будь Потребителем -</span>
+                        <span>Буть Создателем</span>
+                    </h1>
+                    <h2 className={s.subTitle}>
+                        Программирование для детей
+                    </h2>
+                    <div className={s.buttonContainer}>
+                        <div className={s.button}>
+                            Пробный урок
+                        </div>
+                        <div className={s.button}>
+                            Контакты
+                        </div>
+                    </div>
                 </div>
             </header>
         )
